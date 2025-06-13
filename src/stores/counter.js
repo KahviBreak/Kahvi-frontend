63ab8d26653cfb7b419987fab531f6b8266615fe
vallentini <vallentinigabrielli.silva@gmail.com>
2025-06-13 15:52:09 -0300

    feat: tentando fazer o endpoint (falhando miseravelmente)

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,20 +1,32 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-export const useUsuarioStore = defineStore('counter', {
+export const useUsuarioStore = defineStore('usuario', {
   state: () => ({
     isAuthenticated: false,
+    token: null,
+    usuario: null,
     error: null,
   }),
   actions: {
     async login(email, password) {
       try {
-        const response = await axios.get('https://kahvi-back-mgf7.onrender.com/api/usuarios', { email, password })
+        const response = await axios.post(
+          'https://kahvi-back-mgf7.onrender.com/api/login/',
+          {
+            email,
+            password,
+          },
+        )
         this.isAuthenticated = true
+        this.token = response.data.token
+        this.usuario = response.data.usuario || null
         this.error = null
-      } catch (err) {
+      } catch (error) {
         this.isAuthenticated = false
-        this.error = err.response?.data?.error || 'Erro ao fazer login'
+        this.token = null
+        this.usuario = null
+        this.error = error.response?.data?.error || 'Erro ao fazer login'
       }
     },
   },
