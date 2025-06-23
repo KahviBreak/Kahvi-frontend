bf13eb21ab6903dff940ffcb09e3c6b729e7d488
nicoleabrand <nicoleabrand13@gmail.com>
2025-06-23 08:26:50 -0300

    fix: inicio responsividade

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,23 +1,43 @@
 <script setup>
 import '@/assets/main.css'
+import { ref } from 'vue'
+
+const showMobileMenu = ref(false)
+function toggleMenu() {
+    showMobileMenu.value = !showMobileMenu.value
+}
+
 </script>
 
 <template>
     <div id="menu">
-    <img src="@/assets/Kahvi!.png" alt="n deu" id="logo">
+          <div id="togglemenu" @click="toggleMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <img src="@/assets/Kahvi!.png" alt="n deu" id="logo">
+        <div id="menu-links">
             <p>Início</p>
             <p>Sobre nós</p>
             <p>Cardápio</p>
             <p>Faça seu pedido</p>
-            <div id="pesquisa">
+        </div>
+        <div id="pesquisa">
             <h1>Buscar</h1>
             <img src="@/assets/Iconpesquisa.png" alt="n funcionou" id="icon-pesquisa">
-            </div>
-            <div id="icons-end">
-                <img src="@/assets/circle.png" alt="" id="basket">
-                <img src="@/assets/user-profile-03.png" alt="" id="user">
-            </div>
-
+        </div>
+        <div id="icons-end">
+            <img src="@/assets/circle.png" alt="" id="basket">
+            <img src="@/assets/user-profile-03.png" alt="" id="user">
+        </div>
+      
+        <div v-if="showMobileMenu" id="mobile-menu">
+            <p @click="toggleMenu">Início</p>
+            <p @click="toggleMenu">Sobre nós</p>
+            <p @click="toggleMenu">Cardápio</p>
+            <p @click="toggleMenu">Faça seu pedido</p>
+        </div>
     </div>
 </template>
 
@@ -26,10 +46,17 @@ import '@/assets/main.css'
 #menu {
     display: flex;
     flex-direction: row;
+    align-items: center;
     background-color: white;
     margin: 0px;
     height: 100px;
     padding: 0px;
+    position: relative;
+}
+#menu-links {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
 }
 #pesquisa{
     background-color: #5F7B5B;
@@ -40,7 +67,7 @@ import '@/assets/main.css'
     margin-top: 1.8%;
     display: flex;
     flex-direction: row;
-    justify-content: space-between
+    justify-content: space-between;
 }
 #icon-pesquisa{
     height: 24px;
@@ -69,6 +96,7 @@ p{
     font-size: 28px;
     margin-left: 5%;
     margin-top: 2%;
+    cursor: pointer;
 }
 #logo{
     margin-left: 3%;
@@ -81,4 +109,91 @@ p{
     width: 56px;
 }
 
+#togglemenu {
+    display: none;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+    margin-left: 10px;
+}
+#togglemenu span {
+    display: block;
+    width: 30px;
+    height: 4px;
+    background: #5F7B5B;
+    margin: 4px 0;
+    border-radius: 2px;
+    transition: 0.3s;
+}
+#mobile-menu {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 100px;
+    left: 0;
+    width: 100vw;
+    background: white;
+    z-index: 10;
+    box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+}
+#mobile-menu p {
+    margin: 20px;
+    font-size: 24px;
+}
+
+@media (max-width: 900px) {
+    #pesquisa {
+        width: 200px;
+        font-size: 18px;
+    }
+    h1 {
+        font-size: 18px;
+    }
+    #logo {
+        width: 70px;
+        height: 70px;
+    }
+    #basket, #user {
+        width: 36px;
+        height: 36px;
+    }
+    p {
+        font-size: 18px;
+    }
+}
+
+@media (max-width: 700px) {
+    #menu {
+        flex-direction: row;
+        height: auto;
+        padding: 5px 0;
+    }
+    #menu-links, #icons-end {
+        display: none;
+    }
+    #togglemenu {
+        display: flex;
+        margin-left: 50px;
+    }
+    #logo {
+        margin-left: 80px;
+    }
+}
+
+@media (max-width: 400px) {
+    #logo {
+        width: 70px;
+        height: 70px;
+        justify-content: center;
+    }
+    #mobile-menu p {
+        font-size: 18px;
+        display: flex;
+        justify-content: center;
+    }
+}
 </style>
+
