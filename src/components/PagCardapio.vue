b97b7254ce2ddf10a57197aa4dd79434a6a258ec
eduardafh <eduardafheidemann@gmail.com>
2025-06-23 09:12:29 -0300

    feat: comeco front cardapio #17

---
+++ src/components/PagCardapio.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <p class="titulo">UM POUCO SOBRE NOSSO CARDÁPIO...</p>
+    </div>
+
+    <div>
+        <div>
+            <img src="../assets/bolo.png" alt="">
+            <h1>BOLOS</h1>
+            <p>Bolo de Paçoca</p>
+            <p>Bolo Red Velvet</p>
+            <p>Bolo de Laranja</p>
+            <p>Bolo de Chocolate com Brigadeiro</p>
+            <p>Bolo de Banana com Canela</p>
+        </div>
+        <div>
+            <img src="../assets/pao.png" alt="">
+            <h1>SALGADOS</h1>
+            <p>Pão de Queijo</p>
+            <p>Croissant de Presunto e Queijo</p>
+            <p>Sanduíche Natural</p>
+            <p>Focaccia de Alecrim</p>
+            <p>Esfiha Vegana</p>
+        </div>
+        <div>
+            <img src="../assets/cupcake.png" alt="">
+            <h1>SOBREMESAS</h1>
+            <p>Brownie</p>
+            <p>Cheesecake de Frutas Vermelhas</p>
+            <p>Torta de Limão com Merengue</p>
+            <p>Cookie Chocolate</p>
+            <p>Pudim de Leite</p>
+            <p>Mousse de Maracujá</p>
+            <p>Donuts Artesanais (sabores variados)</p>
+        </div>
+        <div>
+            <img src="../assets/xicara.png" alt="">
+            <h1>CAFÈS</h1>
+            <p>Espresso de Brigadeiro</p>
+            <p>Café Coado</p>
+            <p>Macchiato</p>
+            <p>Cappuccino</p>
+            <p>Mocha</p>
+            <p>Latte</p>
+            <p>Affogato</p>
+            <p>Iced Coffee</p>
+            <p>Frappuccino</p>
+        </div>
+    </div>
+
+    <div>
+        <button>Fazer Pedido</button>
+        <img src="../assets/Icon Frame.png" alt="">
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/CardapioView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MenuBar from '@/components/MenuBar.vue';
+import PagCardapio from '@/components/PagCardapio.vue';
+</script>
+
+<template>
+<MenuBar />
+<PagCardapio></PagCardapio>
+</template>
+
+<style scoped>
+
+</style>
