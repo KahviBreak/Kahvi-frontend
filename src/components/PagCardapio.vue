ab765b7d6043bdbbca2e9ec7c9b54e14699bb938
eduardafh <eduardafheidemann@gmail.com>
2025-06-23 16:25:46 -0300

    feat: comeco css #17

--- src/components/PagCardapio.vue
+++ src/components/PagCardapio.vue
@@ -1,57 +1,142 @@
 <template>
-    <div>
-        <p class="titulo">UM POUCO SOBRE NOSSO CARDÁPIO...</p>
-    </div>
+  <div>
+    <p class="titulo">UM POUCO SOBRE NOSSO CARDÁPIO...</p>
+  </div>
 
-    <div>
-        <div>
-            <img src="../assets/bolo.png" alt="">
+  <div class="container">
+    <div class="container-dois">
             <h1>BOLOS</h1>
-            <p>Bolo de Paçoca</p>
-            <p>Bolo Red Velvet</p>
-            <p>Bolo de Laranja</p>
-            <p>Bolo de Chocolate com Brigadeiro</p>
-            <p>Bolo de Banana com Canela</p>
-        </div>
-        <div>
-            <img src="../assets/pao.png" alt="">
+      <div>
+        <img src="../assets/bolo.png" alt="" />
+      </div>
+      <div>
+      <p>Bolo de Paçoca</p>
+      <p>Bolo Red Velvet</p>
+      <p>Bolo de Laranja</p>
+      <p>Bolo de Chocolate com Brigadeiro</p>
+      <p>Bolo de Banana com Canela</p>
+      </div>
+    </div>
+    <div class="container-dois">
             <h1>SALGADOS</h1>
-            <p>Pão de Queijo</p>
-            <p>Croissant de Presunto e Queijo</p>
-            <p>Sanduíche Natural</p>
-            <p>Focaccia de Alecrim</p>
-            <p>Esfiha Vegana</p>
-        </div>
-        <div>
-            <img src="../assets/cupcake.png" alt="">
+      <div>
+        <img src="../assets/pao.png" alt="" />
+      </div>
+      <div>
+      <p>Pão de Queijo</p>
+      <p>Croissant de Presunto e Queijo</p>
+      <p>Sanduíche Natural</p>
+      <p>Focaccia de Alecrim</p>
+      <p>Esfiha Vegana</p>
+      </div>
+    </div>
+    <div class="container-tres">
             <h1>SOBREMESAS</h1>
-            <p>Brownie</p>
-            <p>Cheesecake de Frutas Vermelhas</p>
-            <p>Torta de Limão com Merengue</p>
-            <p>Cookie Chocolate</p>
-            <p>Pudim de Leite</p>
-            <p>Mousse de Maracujá</p>
-            <p>Donuts Artesanais (sabores variados)</p>
-        </div>
-        <div>
-            <img src="../assets/xicara.png" alt="">
+      <div>
+        <img src="../assets/cupcake.png" alt="" />
+      </div>
+      <div>
+      <p>Brownie</p>
+      <p>Cheesecake de Frutas Vermelhas</p>
+      <p>Torta de Limão com Merengue</p>
+      <p>Cookie Chocolate</p>
+      <p>Pudim de Leite</p>
+      <p>Mousse de Maracujá</p>
+      <p>Donuts Artesanais (sabores variados)</p>
+      </div>
+    </div>
+    <div class="container-tres">
             <h1>CAFÈS</h1>
-            <p>Espresso de Brigadeiro</p>
-            <p>Café Coado</p>
-            <p>Macchiato</p>
-            <p>Cappuccino</p>
-            <p>Mocha</p>
-            <p>Latte</p>
-            <p>Affogato</p>
-            <p>Iced Coffee</p>
-            <p>Frappuccino</p>
-        </div>
+      <div>
+        <img src="../assets/xicara.png" alt="" />
+      </div>
+      <div>
+      <p>Espresso de Brigadeiro</p>
+      <p>Café Coado</p>
+      <p>Macchiato</p>
+      <p>Cappuccino</p>
+      <p>Mocha</p>
+      <p>Latte</p>
+      <p>Affogato</p>
+      <p>Iced Coffee</p>
+      <p>Frappuccino</p>
+      </div>
     </div>
+  </div>
 
-    <div>
-        <button>Fazer Pedido</button>
-        <img src="../assets/Icon Frame.png" alt="">
-    </div>
+  <div class="button">
+    <button>Fazer Pedido</button>
+    <img src="../assets/Icon Frame.png" alt="" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.button{
+  display: flex;
+justify-content: center;
+}
+
+button{
+background: #402B19;
+border-radius: 10px;
+color: white;
+width: 10%;
+width: 415px;
+height: 70px;
+border-radius: 10px;
+border: none;
+font-family: 'Varta';
+font-style: normal;
+font-weight: 400;
+font-size: 40px;
+line-height: 58px;
+
+
+}
+
+.titulo {
+  font-family: 'Overlock SC';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 49px;
+  color: #402b19;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+  margin-top: 10%;
+  gap: 100px;
+}
+
+.container-tres {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr 1fr);
+}
+
+.container-dois {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+
+h1 {
+  font-family: 'Overlock SC';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 39px;
+  color: #402b19;
+}
+
+p {
+  font-family: 'Overlock';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+  color: #402b19;
+}
+</style>
