bbdad7d12e3661f2db5a77f41ec5843b41a9bca3
melloyasminn <melloyasminm@gmail.com>
2025-06-30 09:14:28 -0300

    feat:  finalizacao pág desktop #2

--- src/components/PagCadast.vue
+++ src/components/PagCadast.vue
@@ -3,44 +3,87 @@ import '@/assets/main.css'
 </script>
 
 <template>
+
+  <div class="container">
     <img src="@/assets/Group 1.png" alt="" id="g1">
+
     <div id="cadastro">
-        <img src="@/assets/Kahvi!.png" alt="" id="kahvi">
+      <img src="@/assets/Kahvi!.png" alt="" id="kahvi">
+      <div class="inputs">
         <input type="text" placeholder="Nome completo">
-        <input type="text" placeholder="E-mail">
+        <input type="email" placeholder="E-mail">
         <input type="text" placeholder="Nome de usuário">
-        <input type="text" placeholder="Criar senha">
-        <input type="text" placeholder="Confirmar senha">
+        <input type="password" placeholder="Criar senha">
+        <input type="password" placeholder="Confirmar senha">
         <button>CADASTRAR</button>
         <div class="botao-cadastro">
-                    <a href="">Já possuo cadastro
-        </a><img src="@/assets/Group 3.png" alt="" id="img">
+          <a href="">Já possuo cadastro</a>
+          <img src="@/assets/Group 3.png" alt="" id="img">
         </div>
-
+      </div>
     </div>
+
     <img src="@/assets/Group 2.png" alt="" id="g2">
+  </div>
 </template>
+
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Varta:wght@300..700&display=swap');
 
+.container {
+  display: flex;
+flex-direction: row;
+  font-family: "Varta", sans-serif;
+}
+
 #cadastro {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 6%;
+  padding-left: 23%;
+  padding-right: 20%;
+}
+
+#g1{
+    width: 320px;
+    height: 400px;
+}
+
+#g2{
     display: flex;
-    flex-direction: column;
-    padding-left: 38%;
-    padding-right: 38%;
-    font-family: "Varta", sans-serif;
-
-
+        width: 350px;
+    height: 400px;
+position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.inputs {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 300px;
 }
 
 input {
-    margin-top: 5%;
-    padding: 4%;
+    margin-top: 2%;
+    padding: 5%;
     border-radius: 12px;
     border: #BFBDA6 solid 1px;
-    font-size: 16px;
+    font-size: 18px;
+
 }
+button {
+    margin-top: 5%;
+    padding: 4%;
+    border-radius: 7px;
+    border: #2F402C solid 1px;
+    background: #2F402C;
+    color: white;
+    font-size: 20px;
+}
+
 .botao-cadastro{
 display: flex;
 flex-direction: row;
@@ -55,60 +98,25 @@ a {
 
     font-family: "Varta", sans-serif;
     font-size: 20px;
-    margin-top: 2%;
     text-align: center;
     margin-left: 5%;
 }
 
-    #img{
-        margin: 0;
-        margin-top: 5%;
-       height: 17px;
-        width: 31px;
-        margin-left: 0%;
-    }
 
-
-button {
-    margin-top: 5%;
-    padding: 4%;
-    border-radius: 7px;
-    border: #2F402C solid 1px;
-    background: #2F402C;
-    color: white;
-    font-size: 20px;
+#img {
+  height: 17px;
+  width: 31px;
+  margin-top: 2%;
 }
-
 
 #kahvi {
-    width: 354px;
+    width: 180px;
+    margin-bottom: 10%;
+padding-right: 5%;
+    justify-content: center;
+    align-self: center;
+    align-items: center;
 }
 
-#g1 {
-    margin: 0;
-    padding: 0;
-}
 
-#g2 {
-    display: grid;
-    justify-content: end;
-    align-items: end;
-    align-self: flex-end;
-    align-content: end;
-}
-
-h1 {
-    background-color: white;
-    color: #5F7B5B;
-}
-
-p {
-    background-color: white;
-    color: #5F7B5B;
-    font-family: "Varta", sans-serif;
-}
-
-img {
-    margin-left: 41px;
-}
 </style>
