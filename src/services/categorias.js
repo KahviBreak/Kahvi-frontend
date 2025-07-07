466a09b068b8221f29459b4c4387839e5fafe9f3
nicoleabrand <nicoleabrand13@gmail.com>
2025-07-07 16:25:56 -0300

    feat: inicio axios

---
+++ src/services/categorias.js
@@ -0,0 +1,25 @@
+// import axios from 'axios'
+
+// class CategoriaService {
+//   async listarCategorias(pagina = 1, filtro = '') {
+//     try {
+//       const response = await axios.get(`categorias/?page=${pagina}&search=${filtro}`)
+//       return response.data
+//     } catch (error) {
+//       console.error('Error fetching categories:', error)
+//       throw error
+//     }
+//   }
+
+//   async salvarCategoria(categoria) {
+//     try {
+//       await axios.patch(`categorias/${categoria.id}/`, categoria)
+//       return Promise.resolve()
+//     } catch (error) {
+//       console.error('Error saving category API:', error)
+//       return Promise.reject(error.response.data?.detail || 'Erro desconhecido!')
+//     }
+//   }
+// }
+
+// export default CategoriaService
--- src/stores/categorias.js
+++ src/stores/categorias.js
@@ -14,3 +14,45 @@ export const useCategoriaStore = defineStore('categoria', () => {
   return { categorias };
 })
 export default useCategoriaStore
+
+// import { ref, computed } from 'vue'
+// import { defineStore } from 'pinia'
+// import CategoriaService from '@/services/categoria'
+
+// const categoriaService = new CategoriaService()
+
+// export const useCategoriaStore = defineStore('categoria', () => {
+//   const state = ref({
+//     categorias: [],
+//     page: 1,
+//     page_size: 10,
+//     total_pages: 0,
+//   })
+
+//   async function buscarCategorias(pagina = 1, filtro = '') {
+//     const data = await categoriaService.listarCategorias(pagina, filtro)
+//     state.value.categorias = data.results
+//     state.value.total_pages = data.total_pages
+//     state.value.page = data.page
+//     state.value.page_size = data.page_size
+//   }
+
+//   async function salvarCategoria(categoria) {
+//     try {
+//       await categoriaService.salvarCategoria(categoria)
+//       await buscarCategorias()
+//     } catch (error) {
+//       console.error('Error saving category STORE:', error)
+//       return Promise.reject(error)
+//     }
+//   }
+
+//   const categorias = computed(() => state.value.categorias)
+//   const meta = computed(() => ({
+//     page: state.value.page,
+//     page_size: state.value.page_size,
+//     total_pages: state.value.total_pages,
+//   }))
+
+//   return { categorias, meta, buscarCategorias, salvarCategoria }
+// })
