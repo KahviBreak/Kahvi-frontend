e53cdb3a88d3c80cbabc3648c7963e10e9d206d6
melloyasminn <melloyasminm@gmail.com>
2025-07-07 16:41:12 -0300

    feat: pág desktop sobre nós

---
+++ src/components/PagSobre.vue
@@ -0,0 +1,63 @@
+<script setup>
+</script>
+
+<template>
+    <img src="@/assets/Group 31.png" alt="">
+    <div id="txt">
+        <div id="hst">
+            <h1>HISTÓRIA DO KAHVI!</h1>
+            <p>Em 2025, um grupo de amigos apaixonados por café decidiu transformar suas conversas diárias em algo maior. Eles sempre se encontravam em uma charmosa esquina da cidade para compartilhar ideias, risadas e, claro, uma boa xícara de café. </p>
+            <p>Inspirados por esses encontros e pelo desejo de oferecer algo único, nasceu a Kahvi, uma cafeteria que une tradição, inovação e muito sabor.</p>
+            <p>Desde o início, a missão da Kahvi foi clara: oferecer cafés de qualidade superior, com sabores marcantes e aromas que despertam memórias. Mesmo jovem, a marca já chegou inovando, com grãos selecionados de pequenos produtores, opções de cafés aromatizados, métodos de preparo diferenciados e um ambiente acolhedor que convida à pausa e à conexão.</p>
+            <p>Mais do que uma cafeteria, a Kahvi é um ponto de encontro moderno, onde cada detalhe foi pensado para transformar o simples ato de tomar café em uma experiência sensorial. É o começo de uma história que promete marcar presença no coração de quem ama café.</p>
+        </div>
+        <div id="paixao">
+            <h1>PAIXÃO POR CAFÉ</h1>
+            <p>Kahvi! entende que tomar um bom café é um momento especial, e por isso seleciona cuidadosamente os grãos para criar cafés com aromas envolventes e intensos.</p>
+            <p>A marca expressa sua paixão por cafés através de uma grande variedade de blends proporcionando sabores marcantes para serem apreciados por consumidores que buscam por uma qualidade superior, transformando café em uma jornada de paixão.</p>
+            <p>Kahvi, Paixão por café.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300..700&display=swap');
+    img{
+        width: 100%;
+        margin-left: 0%;
+        margin-bottom: 0;
+        padding: 0;
+    }
+    *{
+     font-family: "Spline Sans", sans-serif;
+        background: #402B19;
+        color: white;
+        margin: 0;
+    }
+
+    #txt{
+        margin: 0 !important;
+        display: grid;
+        grid-template-columns: 50% 50%;
+    }
+    #hst, #paixao{
+        margin-left: 15%;
+        margin-right: 15%;
+    }
+
+    #paixao{
+        text-align: center;
+    }
+
+    h1{
+        margin-top: 15%;
+        margin-bottom: 10%;
+        text-align: center;
+    }
+
+    p{
+        margin-bottom: 5%;
+        font-size: 18px;
+         line-height: 1.5;
+    }
+</style>
---
+++ src/views/SobreView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PagSobre from '@/components/PagSobre.vue';
+</script>
+
+<template>
+    <PagSobre/>
+</template>
+
+<style scoped>
+
+</style>
