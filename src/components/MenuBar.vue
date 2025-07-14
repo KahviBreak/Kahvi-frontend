9f145cc94c35d3c0a4c87c6b24425e5f44b2dc07
GustavoCKruger <gustavo.kruger1601@gmail.com>
2025-07-14 14:04:16 -0300

    feat: ajuste css responsividade #3

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -6,17 +6,16 @@ const showMobileMenu = ref(false)
 function toggleMenu() {
     showMobileMenu.value = !showMobileMenu.value
 }
-
 </script>
 
 <template>
     <div id="menu">
-          <div id="togglemenu" @click="toggleMenu">
+        <div id="togglemenu" @click="toggleMenu">
             <span></span>
             <span></span>
             <span></span>
         </div>
-        <img src="@/assets/Kahvi!.png" alt="n deu" id="logo">
+        <img src="@/assets/Kahvi!.png" alt="Logo" id="logo" />
         <div id="menu-links">
             <p>Início</p>
             <p>Sobre nós</p>
@@ -25,13 +24,13 @@ function toggleMenu() {
         </div>
         <div id="pesquisa">
             <h1>Buscar</h1>
-            <img src="@/assets/Iconpesquisa.png" alt="n funcionou" id="icon-pesquisa">
+            <img src="@/assets/Iconpesquisa.png" alt="Buscar" id="icon-pesquisa">
         </div>
         <div id="icons-end">
-            <img src="@/assets/circle.png" alt="" id="basket">
-            <img src="@/assets/user-profile-03.png" alt="" id="user">
+            <img src="@/assets/circle.png" alt="Carrinho" id="basket">
+            <img src="@/assets/user-profile-03.png" alt="Usuário" id="user">
         </div>
-      
+
         <div v-if="showMobileMenu" id="mobile-menu">
             <p @click="toggleMenu">Início</p>
             <p @click="toggleMenu">Sobre nós</p>
@@ -43,68 +42,75 @@ function toggleMenu() {
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Varta:wght@300..700&display=swap');
+
 #menu {
     display: flex;
-    flex-direction: row;
+    flex-wrap: wrap;
     align-items: center;
     background-color: white;
-    margin: 0px;
-    height: 100px;
-    padding: 0px;
+    padding: 10px 20px;
     position: relative;
+    justify-content: space-between;
+    gap: 10px;
 }
+
+#logo {
+    width: 100px;
+    height: 100px;
+}
+
 #menu-links {
     display: flex;
     flex-direction: row;
     flex: 1;
+    justify-content: center;
+    gap: 20px;
+    flex-wrap: wrap;
 }
-#pesquisa{
-    background-color: #5F7B5B;
-    margin-left: 6%;
-    height: 43px;
-    width: 432px;
-    border-radius: 15px;
-    margin-top: 1.8%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-}
-#icon-pesquisa{
-    height: 24px;
-    width: 24px;
-    margin: 2%;
-    margin-right:5%;
-}
-#icons-end{
-    display: flex;
-    gap: 30%;
-    margin-top: 1.5%;
-    margin-left: 4%;
-}
-h1{
-    color: white;
-    font-family: "Varta", sans-serif;
-    font-size: 28px;
-    margin-left: 5%;
-    margin-top: 1%;
-    font-weight: 200;
-}
-p{
+
+#menu-links p,
+#mobile-menu p {
     background-color: white;
     color: #5F7B5B;
     font-family: "Varta", sans-serif;
     font-size: 28px;
-    margin-left: 5%;
-    margin-top: 2%;
+    margin: 0;
     cursor: pointer;
 }
-#logo{
-    margin-left: 3%;
-    width: 100px;
-    height: 100px;
-    margin-top: 1%;
+
+#pesquisa {
+    background-color: #5F7B5B;
+    height: 43px;
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 15px;
+    min-width: 200px;
+    max-width: 432px;
+    flex: 1;
 }
-#basket, #user{
+
+#icon-pesquisa {
+    height: 24px;
+    width: 24px;
+}
+
+#icons-end {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+
+h1 {
+    color: white;
+    font-family: "Varta", sans-serif;
+    font-size: 28px;
+    font-weight: 200;
+    margin: 0;
+}
+
+#basket, #user {
     height: 56px;
     width: 56px;
 }
@@ -117,8 +123,8 @@ p{
     width: 40px;
     height: 40px;
     cursor: pointer;
-    margin-left: 10px;
 }
+
 #togglemenu span {
     display: block;
     width: 30px;
@@ -128,72 +134,64 @@ p{
     border-radius: 2px;
     transition: 0.3s;
 }
+
 #mobile-menu {
     display: flex;
     flex-direction: column;
     position: absolute;
-    top: 100px;
+    top: 100%;
     left: 0;
-    width: 100vw;
+    width: 100%;
     background: white;
     z-index: 10;
     box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+    padding: 10px 0;
 }
+
 #mobile-menu p {
-    margin: 20px;
-    font-size: 24px;
+    padding: 10px 20px;
+    font-size: 20px;
+    text-align: center;
 }
 
 @media (max-width: 900px) {
-    #pesquisa {
-        width: 200px;
-        font-size: 18px;
-    }
-    h1 {
-        font-size: 18px;
+    h1, p {
+        font-size: 22px;
     }
     #logo {
-        width: 70px;
-        height: 70px;
+        width: 80px;
+        height: 80px;
     }
     #basket, #user {
-        width: 36px;
-        height: 36px;
+        width: 42px;
+        height: 42px;
     }
-    p {
-        font-size: 18px;
+    #pesquisa {
+        width: 60%;
     }
 }
 
-@media (max-width: 700px) {
-    #menu {
-        flex-direction: row;
-        height: auto;
-        padding: 5px 0;
-    }
-    #menu-links, #icons-end {
+@media (max-width: 1300px) {
+    #menu-links,
+    #icons-end {
         display: none;
     }
     #togglemenu {
         display: flex;
-        margin-left: 50px;
     }
     #logo {
-        margin-left: 80px;
+        margin-left: auto;
+        margin-right: auto;
     }
 }
 
-@media (max-width: 400px) {
+@media (max-width: 500px) {
     #logo {
-        width: 70px;
-        height: 70px;
-        justify-content: center;
+        width: 60px;
+        height: 60px;
     }
-    #mobile-menu p {
+    h1, #mobile-menu p {
         font-size: 18px;
-        display: flex;
-        justify-content: center;
     }
 }
 </style>
-
