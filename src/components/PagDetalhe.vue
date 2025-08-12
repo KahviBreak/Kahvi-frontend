9d8a48d673e2c3cc0fe18c17194fb65736f70158
eduardafh <eduardafheidemann@gmail.com>
2025-08-12 14:53:19 -0300

    feat: finalizado #21

--- src/components/PagDetalhe.vue
+++ src/components/PagDetalhe.vue
@@ -59,15 +59,15 @@ const diminuirQuantidade = () => {
             d="M9 0C13.9657 0 18 3.72236 18 8.32416C18 12.926 13.9657 16.6483 9 16.6483C7.58962 16.6483 6.25294 16.3477 5.06271 15.8114L1.73633 16.9457C1.7009 16.9578 1.66461 16.9676 1.62774 16.9751C0.792068 17.145 0.0636139 16.4179 0.311432 15.6527C0.319403 15.6281 0.32869 15.6039 0.339258 15.5801L1.51222 12.9439C0.557316 11.6223 0 10.0327 0 8.32416C0 3.72236 4.03428 0 9 0ZM12.7385 7.72701C13.1973 7.72701 13.5692 8.07296 13.5692 8.49971C13.5692 8.92646 13.1973 9.27242 12.7385 9.27242H12.6C12.1412 9.27242 11.7692 8.92646 11.7692 8.49971C11.7692 8.07296 12.1412 7.72701 12.6 7.72701H12.7385ZM9.96923 8.49971C9.96923 8.07296 9.59728 7.72701 9.13846 7.72701H9C8.54118 7.72701 8.16923 8.07296 8.16923 8.49971C8.16923 8.92646 8.54118 9.27242 9 9.27242H9.13846C9.59728 9.27242 9.96923 8.92646 9.96923 8.49971ZM5.53846 7.72701C5.99728 7.72701 6.36923 8.07296 6.36923 8.49971C6.36923 8.92646 5.99728 9.27242 5.53846 9.27242H5.4C4.94118 9.27242 4.56923 8.92646 4.56923 8.49971C4.56923 8.07296 4.94118 7.72701 5.4 7.72701H5.53846Z"
             fill="#6E5641" />
         </svg>
-        alguma observação?</label>
+        Alguma observação?</label>
       <input type="text" placeholder="Ex: tirar azeitona, milho etc.">
       </div>
       <div class="botoes">
         <div>
           <button class="botao">Comprar agora</button>
-        </div>
-        <div>
+
           <button style="background-color: #C2E0BD; color:#5F7B5B;" class="botao">Adicionar no carrinho</button>
+
         </div>
       </div>
     </div>
@@ -224,15 +224,14 @@ input[type="text"] {
   cursor: pointer;
   width: 100%;
   margin-top: 8px;
-
 font-family: 'Overpass';
 font-style: normal;
 font-weight: 600;
 font-size: 24px;
 line-height: 30px;
-
 color: #FFFFFF;
-
+  align-items: center;
+  text-align: center;
 
 }
 
@@ -397,7 +396,6 @@ input[type="text"] {
 .botoes {
   display: flex;
   flex-direction: column;
-  gap: 1px;
 }
 
 .botao {
@@ -407,8 +405,8 @@ input[type="text"] {
   font-size: 14px;
   font-weight: bold;
   cursor: pointer;
-  width: 70%;
-  height:80%;
+  width: 60%;
+  height:30%;
   display: flex;
   align-items: center;
   text-align: center;
