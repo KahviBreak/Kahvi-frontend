b1dd8e473b46f75ec4602cfcc175a16cb040d564
eduardafh <eduardafheidemann@gmail.com>
2025-08-12 14:41:46 -0300

    feat: css finalizado #21

--- src/components/PagDetalhe.vue
+++ src/components/PagDetalhe.vue
@@ -52,7 +52,8 @@ const diminuirQuantidade = () => {
           <input type="number" name="grao" id="grao">
         </div>
       </div>
-      <p class="preco"> r$3737874</p>
+      <p class="preco"> R$XX</p>
+      <div>
       <label class="obs"><svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd"
             d="M9 0C13.9657 0 18 3.72236 18 8.32416C18 12.926 13.9657 16.6483 9 16.6483C7.58962 16.6483 6.25294 16.3477 5.06271 15.8114L1.73633 16.9457C1.7009 16.9578 1.66461 16.9676 1.62774 16.9751C0.792068 17.145 0.0636139 16.4179 0.311432 15.6527C0.319403 15.6281 0.32869 15.6039 0.339258 15.5801L1.51222 12.9439C0.557316 11.6223 0 10.0327 0 8.32416C0 3.72236 4.03428 0 9 0ZM12.7385 7.72701C13.1973 7.72701 13.5692 8.07296 13.5692 8.49971C13.5692 8.92646 13.1973 9.27242 12.7385 9.27242H12.6C12.1412 9.27242 11.7692 8.92646 11.7692 8.49971C11.7692 8.07296 12.1412 7.72701 12.6 7.72701H12.7385ZM9.96923 8.49971C9.96923 8.07296 9.59728 7.72701 9.13846 7.72701H9C8.54118 7.72701 8.16923 8.07296 8.16923 8.49971C8.16923 8.92646 8.54118 9.27242 9 9.27242H9.13846C9.59728 9.27242 9.96923 8.92646 9.96923 8.49971ZM5.53846 7.72701C5.99728 7.72701 6.36923 8.07296 6.36923 8.49971C6.36923 8.92646 5.99728 9.27242 5.53846 9.27242H5.4C4.94118 9.27242 4.56923 8.92646 4.56923 8.49971C4.56923 8.07296 4.94118 7.72701 5.4 7.72701H5.53846Z"
@@ -60,6 +61,7 @@ const diminuirQuantidade = () => {
         </svg>
         alguma observação?</label>
       <input type="text" placeholder="Ex: tirar azeitona, milho etc.">
+      </div>
       <div class="botoes">
         <div>
           <button class="botao">Comprar agora</button>
@@ -277,7 +279,6 @@ color: #FFFFFF;
   }
 }
 
-/* Ajuste para celulares */
 @media (max-width: 600px) {
   .container {
   max-width: 450px;
@@ -315,7 +316,7 @@ h1 {
 }
 
 .descricao {
-  font-size: 14px;
+  font-size: 16px;
   color: #93755C;
   margin: 0;
 }
@@ -325,12 +326,14 @@ h1 {
   font-weight: bold;
   color: #556B4E;
   margin: 0;
+  margin-top: 3%;
 }
 
 .container-quant {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-top: 3%;
 }
 
 .quantidade, .graos {
@@ -394,7 +397,7 @@ input[type="text"] {
 .botoes {
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 1px;
 }
 
 .botao {
@@ -406,8 +409,11 @@ input[type="text"] {
   cursor: pointer;
   width: 70%;
   height:80%;
-  justify-self: center;
   display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  justify-self: center;
 }
 }
 </style>
