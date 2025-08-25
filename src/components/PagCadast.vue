3b164dd1215cabac61a58245bba5c6afe6de6754
vallentini <vallentinigabrielli.silva2gmail.com>
2025-08-25 15:45:09 -0300

    feat: responsividade da página cadastro concluída #30

--- src/components/PagCadast.vue
+++ src/components/PagCadast.vue
@@ -3,158 +3,134 @@ import '@/assets/main.css'
 </script>
 
 <template>
-    <div class="container">
-        <img src="@/assets/Group 1.png" alt="" id="g1" class="desktop-only">
+  <div class="container">
+    <img src="@/assets/Group 1.png" alt="" id="g1" class="desktop-only">
 
-        <div id="cadastro">
-            <img src="@/assets/Kahvi!.png" alt="" id="kahvi">
-            <div class="inputs">
-                <input type="text" placeholder="Nome completo">
-                <input type="email" placeholder="E-mail">
-                <input type="text" placeholder="Nome de usuário">
-                <input type="password" placeholder="Criar senha">
-                <input type="password" placeholder="Confirmar senha">
-                <button>CADASTRAR</button>
-                <div class="botao-cadastro">
-                    <a href="">Já possuo cadastro</a>
-                    <img src="@/assets/Group 3.png" alt="" id="img">
-                </div>
-            </div>
+    <div id="cadastro">
+      <img src="@/assets/Kahvi!.png" alt="" id="kahvi">
+      <div class="inputs">
+        <input type="text" placeholder="Nome completo">
+        <input type="email" placeholder="E-mail">
+        <input type="text" placeholder="Nome de usuário">
+        <input type="password" placeholder="Criar senha">
+        <input type="password" placeholder="Confirmar senha">
+        <button>CADASTRAR</button>
+        <div class="botao-cadastro">
+          <a href="">Possuo cadastro</a>
+          <img src="@/assets/Group 3.png" alt="" id="img">
         </div>
-
-        <img src="@/assets/Group 2.png" alt="" id="g2" class="desktop-only">
+      </div>
     </div>
+
+    <img src="@/assets/Group 2.png" alt="" id="g2" class="desktop-only">
+  </div>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Varta:wght@300..700&display=swap');
 
 .container {
-    display: flex;
-    flex-direction: row;
-    font-family: "Varta", sans-serif;
+  display: flex;
+  justify-content: center;   
+  align-items: center;       
+  min-height: 100vh;         
+  font-family: "Varta", sans-serif;
+  position: relative;
+  background: #fff;
 }
 
 #cadastro {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 6%;
-    padding-left: 23%;
-    padding-right: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  z-index: 1; 
 }
 
 #g1 {
-    width: 320px;
-    height: 400px;
+  width: 320px;
+  height: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  
 }
 
 #g2 {
-    display: flex;
-    width: 350px;
-    height: 400px;
-    position: absolute;
-    bottom: 0;
-    right: 0;
+  width: 350px;
+  height: auto;
+  position: absolute;
+  bottom: 0;
+  right: 0;
 }
 
 .inputs {
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: 300px;
 }
 
 input {
-    margin-top: 2%;
-    padding: 5%;
-    border-radius: 12px;
-    border: #BFBDA6 solid 1px;
-    font-size: 18px;
+  padding: 12px;
+  border-radius: 12px;
+  border: #BFBDA6 solid 1px;
+  font-size: 18px;
 }
 
 button {
-    margin-top: 5%;
-    padding: 4%;
-    border-radius: 7px;
-    border: #2F402C solid 1px;
-    background: #2F402C;
-    color: white;
-    font-size: 20px;
+  margin-top: 10px;
+  padding: 12px;
+  border-radius: 7px;
+  border: #2F402C solid 1px;
+  background: #2F402C;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
 }
 
 .botao-cadastro {
-    display: flex;
-    flex-direction: row;
-    align-self: center;
-    white-space: nowrap;
-    margin-top: 2%;
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+  margin-top: 10px;
 }
-
 a {
-    margin: 0;
-    text-decoration: none;
-    color: #2F402C;
-    font-family: "Varta", sans-serif;
-    font-size: 20px;
-    text-align: center;
-    margin-left: 5%;
+  text-decoration: none;
+  color: #2F402C;
+  font-family: "Varta", sans-serif;
+  font-size: 16px;
+  margin-right: 5px;
 }
 
 #img {
-    height: 17px;
-    width: 31px;
-    margin-top: 2%;
+  height: 17px;
+  width: 31px;
 }
 
 #kahvi {
-    width: 180px;
-    margin-bottom: 10%;
-    padding-right: 5%;
-    justify-content: center;
-    align-self: center;
-    align-items: center;
+  width: 180px;
+  margin-bottom: 30px;
 }
 
 @media (max-width: 768px) {
-    .container {
-        flex-direction: column;
-        padding: 20px;
-        position: relative;
-    }
+  .container {
+    flex-direction: column;
+    padding: 20px;
+  }
 
-    #cadastro {
-        padding-left: 0;
-        padding-right: 0;
-        margin-top: 10px;
-        width: 100%;
-    }
+  .inputs {
+    width: 90%;
+  }
 
-    .inputs {
-        width: 90%;
-    }
+  #kahvi {
+    width: 150px;
+  }
 
-    input {
-        padding: 12px;
-        font-size: 16px;
-    }
-
-    button {
-        font-size: 18px;
-        padding: 12px;
-    }
-
-    #kahvi {
-        width: 150px;
-        margin-bottom: 20px;
-        padding-right: 0;
-        margin-top: 10%;
-        margin-bottom: 10%;
-    }
-
-    #g1,
-    #g2 {
-        display: none;
-    }
+  #g1,
+  #g2 {
+    display: none;
+  }
 }
 </style>
