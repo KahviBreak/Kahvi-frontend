0853bcada10aab535157b3fca1cd9c6f030375c1
melloyasminn <melloyasminm@gmail.com>
2025-09-06 18:51:43 -0300

    FEAT: pág. localização finalizada #55

---
+++ src/components/PagLocalizacao.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="localizacao-container">
+    <!-- Banner -->
+    <section class="banner">
+      <img src="@/assets/banner.png" alt="Cafeteria Kahvi" />
+    </section>
+
+    <!-- Conteúdo -->
+    <section class="content">
+      <h2 class="titulo">Joinville - SC</h2>
+      <div class="linha-title"></div>
+
+
+      <div class="grid">
+        <!-- Informações -->
+        <div class="info">
+          <div class="bloco">
+            <div class="titulo-info">
+<img src="@/assets/marker-02.png" alt="" class="marker">
+            </div>
+            <h3>Onde estamos</h3>
+            <p>Rua dos Abacates, Glória - 111</p>
+            <p style="margin-bottom: 8%;">Joinville/SC</p>
+
+            <div style="display: flex;">
+              <img src="@/assets/mail-03.png" alt="" style="width: 25px; height: 25px;">
+            <p style="margin-left: 1%;">kahvicafeteria@gmail.com</p>
+            </div>
+            <div style="display: flex;">
+            <img src="@/assets/phone-call-01.png" alt="" style="width: 25px; height: 25px;">
+            <p style="margin-left: 1%;">+55 (47) 4002-8922</p>
+            </div>
+          </div>
+
+          <div class="bloco">
+            <div class="titulo-info">
+              <img src="@/assets/icon.png" alt="" class="marker02">
+            </div>
+            <strong>Horários de funcionamento</strong>
+            <p>Seg. a Sáb.: 8h às 23h</p>
+            <p>Dom.: 12h às 22h</p>
+          </div>
+        </div>
+
+        <!-- Fotos -->
+        <div class="fotos">
+          <img src="@/assets/fachada.png" alt="Fachada da cafeteria" />
+          <img src="@/assets/interior.png" alt="Interior da cafeteria" />
+        </div>
+      </div>
+
+      <!-- Linha separadora -->
+      <div class="linha"></div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Tai+Le&display=swap');
+
+p{
+   font-family: "Noto Sans Tai Le", sans-serif;
+  font-weight: 100 !important;
+  color: rgba(255, 255, 255, 0.699);
+}
+
+.localizacao-container {
+  font-family: Arial, sans-serif !important;
+  background-color: #2f3e2f;
+  color: #fff;
+}
+
+/* Banner */
+.banner img {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+
+.marker02{
+  width: 30px;
+  background: rgba(95, 123, 91, 0.4);
+  padding: 3%;
+  border-radius: 2px;
+  margin-bottom: 1%;
+}
+
+.marker{
+  width: 40px;
+  background: rgba(95, 123, 91, 0.4);
+  padding: 2%;
+  border-radius: 2px;
+  margin-bottom: 1%;
+}
+
+/* Conteúdo */
+.content {
+  background-color: #2f3e2f;
+  padding: 40px 20px;
+  margin-left: 5%;
+  margin-top: 1%;
+}
+
+.titulo {
+  font-size: 28px;
+  margin-bottom: 30px;
+  color: #f5f0e6;
+}
+
+/* Grid desktop */
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 40px;
+  align-items: start;
+}
+
+/* Informações */
+.info {
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  font-size: 16px;
+  line-height: 1.4;
+}
+
+.bloco {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.titulo-info {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.icon {
+  font-size: 18px;
+}
+
+/* Fotos */
+.fotos {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: -18%
+}
+
+.fotos img {
+  width: 80%;
+  border-radius: 6px;
+  object-fit: cover;
+}
+
+/* Linha separadora */
+.linha {
+  margin-top: 40px;
+  border-top: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+.linha-title{
+  border-top: 2px solid rgba(255, 255, 255, 0.2);
+  margin-bottom: 5%;
+  width: 15%;
+  margin-top: -1%;
+}
+
+/* Responsivo */
+@media (max-width: 768px) {
+  .grid {
+    grid-template-columns: 1fr;
+  }
+
+  .titulo {
+    text-align: left;
+  }
+
+
+  .linha-title{
+    display: none;
+  }
+  .info {
+    text-align: left;
+    align-items: left;
+  }
+
+  .titulo-info {
+    justify-content: left;
+  }
+
+  .fotos img {
+    max-width: 90%;
+    margin: 0 auto !important;
+    border-radius: 2px !important;
+  }
+
+  .fotos{
+    margin-top: 3%;
+    margin-left: 0 !important;
+    margin-right: 7%;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import SobreView from '@/views/SobreView.vue'
 import PedidoView from '@/views/PedidoView.vue'
 import CardapioView from '@/views/CardapioView.vue'
 import PagCarrinhoView from '@/views/PagCarrinhoView.vue'
+import LocalizacaoView from '@/views/LocalizacaoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -52,6 +53,11 @@ const router = createRouter({
       name: 'carrinho',
       component: PagCarrinhoView
     },
+    {
+      path: '/localizacao',
+      name: 'localizacao',
+      component: LocalizacaoView
+    }
   ]});
 
 export default router
