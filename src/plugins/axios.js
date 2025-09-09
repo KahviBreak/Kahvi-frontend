ae5dca7daf1689d964ec838c35efeac61883d4df
nicoleabrand <nicoleabrand13@gmail.com>
2025-09-09 14:56:13 -0300

    feat: login com passage concluído #39

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,21 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:19003/api'
 
-// const token = localStorage.getItem('psg_auth_token')
-// if (token) {
-//   axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('psg_auth_token')}`
-// }
+const token = localStorage.getItem('psg_auth_token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('psg_auth_token')}`
+}
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('psg_auth_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default axios;
