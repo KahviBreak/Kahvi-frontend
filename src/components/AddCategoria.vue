d0944ad8492951f85a5bbae7980367631557617b
eduardafh <eduardafheidemann@gmail.com>
2025-09-15 14:19:48 -0300

    feat: desktop pronto #74

--- src/components/AddCategoria.vue
+++ src/components/AddCategoria.vue
@@ -1,25 +1,60 @@
 <template>
+  <div></div>
+  <div class="voltar">
+      <router-link to="/">
+        <img src="/src/assets/voltar.png" alt="" />
+      </router-link>
+    </div>
     <div class="container">
+      <div class="teste">
         <h1> Adicionar Categoria</h1>
         <div class="input">
             <label for="">Nome categoria</label>
             <input type="text" name="" id="" placeholder="Ex: bolos">
         </div>
         <div class="buttons">
-            <button>Salvar Categoria</button>
-            <button>Remover Categoria</button>
+            <button class="button-um">Salvar Categoria</button>
+            <button class="button-dois">Remover Categoria</button>
         </div>
+    </div>
     </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Overlock:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Overpass:ital,wght@0,100..900;1,100..900&family=Yantramanav:wght@100;300;400;500;700;900&display=swap');
 
 
+.container{
+  display: flex;
+  justify-content: center;
+}
+
+.teste{
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+.voltar {
+  align-self: flex-start;
+  margin-left: 30px;
+}
+
+h1{
+  margin-top: 30%;
+font-family: 'Overlock';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+color: #402B19;
+display: flex;
+
+}
 
 label {
   font-weight: 500;
   font-size: 18px;
   color: #5F7B5B;
+  margin-top: 6%;
 }
 
 input[type="text"] {
@@ -42,22 +77,43 @@ input[type="text"] {
 .buttons{
     display: grid;
     grid-template-columns: 1fr;
+    margin-top: 10%;
 }
 
-button {
-  background-color: #556B4E;
+.button-um {
+  background-color: #C2E0BD;
+  color: #5F7B5B;
+  border: none;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  width: 80%;
+  font-family: 'Overpass';
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 50px;
+  display: flex;
+  justify-self: center;
+  justify-content: center;
+}
+
+.button-dois {
+  background-color: #FF0000;
   color: white;
   border: none;
   border-radius: 8px;
   font-size: 16px;
   cursor: pointer;
-  width: 10%;
+  width: 80%;
   font-family: 'Overpass';
   font-weight: 600;
   font-size: 18px;
-  line-height: 30px;
+  line-height: 50px;
   color: #FFFFFF;
-  align-items: center;
-  text-align: center;
+  margin-top: 2%;
+    display: flex;
+  justify-self: center;
+  justify-content: center;
+  
 }
 </style>
