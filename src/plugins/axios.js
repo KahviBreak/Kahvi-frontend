534e37f91fd8c231b5d7b29ac4787cd38701146f
nicoleabrand <nicoleabrand13@gmail.com>
2025-09-29 15:41:24 -0300

    feat: inicio ajustes menu #90

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://kahvi-back.onrender.com/api'
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 const token = localStorage.getItem('psg_auth_token')
 if (token) {
