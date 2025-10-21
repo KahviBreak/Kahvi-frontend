4a8edd11e65c8d4aa713e6bb1656c9e06e4a69f3
melloyasminn <melloyasminm@gmail.com>
2025-10-21 14:20:47 -0300

    feat pronto #97

---
+++ src/views/StatusAdminView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MenuBar from '@/components/MenuBar.vue';
+import StatusPedidoAdmin from '@/components/StatusPedidoAdmin.vue';
+import FooterBar from '@/components/FooterBar.vue';
+</script>
+
+<template>
+  <MenuBar/>
+    <StatusPedidoAdmin/>
+    <FooterBar/>
+</template>
+
+<style scoped>
+
+</style>
